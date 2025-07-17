7dc0187e1c998535b0d6c26cd03ae53176c78906
moldabekova_59329 <akbota.moldabekova@kaspi.kz>
2025-07-17 12:03:11 +0500

    new upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,30 @@
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-    });
-}
-
-window.addEventListener("scroll", function () {
+document.addEventListener("DOMContentLoaded", () => {
+    const btnUp = document.getElementById("btnUp");
+    const openHeaderBtn = document.getElementById("openHeaderBtn");
+    const closeHeaderBtn = document.getElementById("closeHeaderBtn");
     const header = document.querySelector(".header");
+    const menu = document.querySelector(".header__menu");
+    const menuNav = document.querySelector(".header__menu-nav");
 
-    if (window.scrollY > 0) {
-        header.classList.add("scrolled");
-    } else {
-        header.classList.remove("scrolled");
-    }
+    const toggleMenu = () => {
+        if (!menu) return;
+        menu.classList.toggle("active");
+        document.body.classList.toggle("lock");
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({top: 0, behavior: "smooth"});
+    };
+
+    const handleScroll = () => {
+        if (!header) return;
+        header.classList.toggle("scrolled", window.scrollY > 0);
+    };
+
+
+    btnUp?.addEventListener("click", scrollToTop);
+    openHeaderBtn?.addEventListener("click", toggleMenu);
+    closeHeaderBtn?.addEventListener("click", toggleMenu);
+    menuNav.addEventListener("click", toggleMenu);
+    window.addEventListener("scroll", handleScroll);
 });
-
-function openMenu() {
-    const menu = document.querySelector(".header__menu");
-    menu.classList.toggle("active")
-    document.body.classList.toggle("lock");
-}
-
-function closeMenu() {
-    const menu = document.querySelector(".header__menu");
-    menu.classList.toggle("active")
-    document.body.classList.toggle("lock");
-}
